74597fde1b8c05e0bc56ab638db5e661d0d86a96
admin <sinkibo@gmail.com>
2018-01-13 15:14:52 +0700

    Code WoWonder 1.3

---
+++ config.php
@@ -0,0 +1,25 @@
+<?php
+// +------------------------------------------------------------------------+
+// | @author Deen Doughouz (DoughouzForest)
+// | @author_url 1: http://www.wowonder.com
+// | @author_url 2: http://codecanyon.net/user/doughouzforest
+// | @author_email: wowondersocial@gmail.com   
+// +------------------------------------------------------------------------+
+// | WoWonder - The Ultimate PHP Social Networking Platform
+// | Copyright (c) 2016 WoWonder. All rights reserved.
+// +------------------------------------------------------------------------+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "root";
+// MySQL Database Password
+$sql_db_pass = "root";
+// MySQL Database Name
+$sql_db_name = "wowonder_db";
+
+// Site URL
+$site_url = "http://localhost:8888/wowonder"; // e.g (http://example.com)
+
+// Purchase code
+$purchase_code = "611bc5c0-a09d-4621-8c66-9bf957013415"; // Your purchase code, don't give it to anyone. 
+?>
